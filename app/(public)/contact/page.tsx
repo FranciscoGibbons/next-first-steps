34d95d8bc34773c9dd0c2481f5e7e2278bbd18e2
Francisco Gibbons <franchugibbdev@gmail.com>
2024-03-18 21:53:36 -0300

    first commit

---
+++ app/(public)/contact/page.tsx
@@ -0,0 +1,8 @@
+
+export default function ContactPage() {
+  return (
+    <div>
+      <h1 className="text-4xl">Hello Page</h1>
+    </div>
+  );
+}
---
+++ app/(public)/pricing/page.tsx
@@ -0,0 +1,8 @@
+
+export default function PricingPage() {
+  return (
+    <div>
+      <h1 className="text-4xl">Pricing</h1>
+    </div>
+  );
+}
